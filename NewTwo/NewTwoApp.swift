c45691d78664e3ad6d467307954f8e769530f918
Shawn Tucker <shqtucke@gmail.com>
2024-05-10 20:58:49 -0400

    Initial Commit

---
+++ NewTwo/NewTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTwoApp.swift
+//  NewTwo
+//
+//  Created by Shawn Tucker on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
